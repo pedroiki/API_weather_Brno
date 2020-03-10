8e52e2985dbd70daacb99086428a70c88ecdad6e
Carlos Pedro Gomes <57367344+pedroiki@users.noreply.github.com>
2020-03-10 22:50:34 +0100

    Server side done ,is working
    Server side done ,is working , now need to be had the html

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 const express = require("express");
-const https   = require("https");
+const https = require("https");
 
 const app = express();
 
-app.get("/", function(req, res) {
-// made with postman and open weather website
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Brno&appid=4c3ecc1a8f3294ae04e769ca473aa51b&units=metric";
+app.get("/", function(req, res) {  // made with postman and open weather website
 
-https.get(url, function(response) {
-  console.log(response.statusCode);         //stateCode tell us if sucess or not positive 200 or 404 for errors
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Brno&appid=4c3ecc1a8f3294ae04e769ca473aa51b&units=metric";
 
-    response.on("data", function(data){
-        const weatherData = JSON.parse(data)
-        const temp = weatherData.main.feels_like
-        const weatherDescription = weatherData.weather[0].description    //0 because is a array
-        console.log(weatherDescription);
+  https.get(url, function(response) {
+      console.log(response.statusCode); //stateCode tell us if sucess or not positive 200 or 404 for errors
 
-
-    })
+      response.on("data", function(data) {
+          const weatherData = JSON.parse(data)
+          const temp = weatherData.main.temp
+          const weatherDescription = weatherData.weather[0].description  //0 because is a array
+          const icon = weatherData.weather[0].icon
+          const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+          res.write("<p>The weather is currently " + weatherDescription + "<p>");
+          res.write("<h1>The Temperature for Brno now is" + temp + "degrees Celcius.<h1>");
+          res.write("<img src=" + imageURL +">");
+          res.send()   // res.send and res.write are similar , we can only use 1 per project so use boths
+      })
+  })
 })
 
-  res.send("server is up and running.")
-})
 
 
-
-
-
-app.listen(3000, function () {
-    console.log("server is running on port 3000.");
+app.listen(3000, function() {
+  console.log("server is running on port 3000.");
 })
